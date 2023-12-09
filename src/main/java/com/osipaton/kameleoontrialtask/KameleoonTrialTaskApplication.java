7a2d248440198daead7d1466f953ac2035433a9e
osipation <nik.79567@gmail.com>
2023-12-09 16:20:24 +0300

    init commit

---
+++ src/main/java/com/osipaton/kameleoontrialtask/KameleoonTrialTaskApplication.java
@@ -0,0 +1,13 @@
+package com.osipaton.kameleoontrialtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KameleoonTrialTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KameleoonTrialTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osipaton/kameleoontrialtask/KameleoonTrialTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osipaton.kameleoontrialtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KameleoonTrialTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
